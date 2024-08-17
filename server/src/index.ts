1630c7eb86f3850d7aff6782cae6a37dc7d4880d
blackcode1996 <aakashmukherjee1996@gmail.com>
2024-08-17 15:20:30 +0530

    bug_fix

--- server/src/index.ts
+++ server/src/index.ts
@@ -13,7 +13,7 @@ app.use(cors());
 app.use(express.json());
 
 
-app.use(express.static(path.join(__dirname, '../../client/build')));
+app.use(express.static(path.join(__dirname, '../../client/dist')));
 
 
 connectMongoDB();
